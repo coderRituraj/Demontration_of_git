33dc2c3551a01e1c3d2b2a1e09de6f36fe492d89
Rituraj Singh <Rituraj.Singh@bhavnacorp.com>
2022-08-01 15:53:28 +0530

    First demo project

---
+++ c++_progrm.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using name space std;
+
+int main(){
+
+int a, b, c;
+cout<<"enter firts ";
+cin>>a;
+cout<<"enter the second number";
+cin>>b;
+c= a+b;
+cout<<"sum of two number is "<<c;
+return 0;
+}
